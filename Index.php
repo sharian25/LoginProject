70ade02cf18e8e342d1dd02d47e727ca7a2d2312
pedroAntonio <pedro.salinas25@aol.com>
2023-09-12 21:18:17 -0500

    se inicia miniProyecto DevLeve_3

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>mini_Project</title>
+</head>
+<body>
+    <div class="join">
+    <h2>Join thousands of learnes from aroun the word</h2>
+    <p>Master web develoope ment by making real-life project. There are multiple paths for you to choose</p>
+    <input type="text" placeholder="Email"> 
+    <input type="text" placeholder="Password"> 
+    <button type="submit">Star coding now</button> 
+    </div>
+    
+
+</body>
+</html>
